ccaf5b8c082bf1ef95bce50e759620bbced4242c
e-mail <71135@student.ue.poznan.pl>
2022-06-14 03:08:24 +0200

    Trzecia wersja

--- main.py
+++ main.py
@@ -31,8 +31,9 @@ for lista in figury:
     wynik=(poleProstokata(lista))
   elif len(lista)==3:
     wynik=(poleTrojkata(lista))
-suma=suma+wynik
+
+  suma=suma+wynik
 print(zaokr(suma,2)) 
 for lista in figury:  
   if len(lista)>3:
-    print("Można podać maksymalnie trzy liczby")
+    print("Błąd: można podać maksymalnie trzy liczby")
